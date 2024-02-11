f7b0e8e37871c93a19b0eec566b879d788c6ffa7
Kris1027 <obarzanek.work@gmail.com>
2024-02-11 12:06:06 +0100

    build with functionality burger menu

---
+++ components/app/page.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Nav from "./burger-menu/Nav";
+
+export default function Home() {
+  return (
+    <main className="p-28">
+      <Nav />
+    </main>
+  );
+}
